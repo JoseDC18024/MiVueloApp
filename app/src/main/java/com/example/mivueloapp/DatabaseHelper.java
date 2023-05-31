837a881fbc95cbbcc152eb49451c04a71bdf87df
KevinVillM <89365161+KevinVillM@users.noreply.github.com>
2023-05-31 16:45:39 -0600

    Se agrego la tabla aerolinea

--- app/src/main/java/com/example/mivueloapp/DatabaseHelper.java
+++ app/src/main/java/com/example/mivueloapp/DatabaseHelper.java
@@ -12,6 +12,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String CREATE_TABLE_CUPO = "CREATE TABLE IF NOT EXISTS cupo (id_cupo CHAR(11) NOT NULL, " +
             "cantidad_cupo INTEGER NOT NULL);";
 
+    // Sentencia SQL para crear la tabla "aerolinea"
+    private static final String CREATE_TABLE_AEROLINEA = "CREATE TABLE IF NOT EXISTS aerolinea (id_aerolinea CHAR(11) PRIMARY KEY, " +
+            "nombre_aerolinea CHAR(30) NOT NULL, pais_aerolinea CHAR(50) NOT NULL, fecha_aerolinea CHAR(20) NOT NULL);";
+
     // Sentencia SQL para crear la tabla "avion"
     private static final String CREATE_TABLE_AVION = "CREATE TABLE IF NOT EXISTS avion (id_avion CHAR(11) PRIMARY KEY, " +
             "modelo_avion CHAR(30) NOT NULL, año_fabricacion INTEGER NOT NULL);";
@@ -52,6 +56,122 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String CREATE_TABLE_TRIPULANTE = "CREATE TABLE tripulante (id_tripulante CHAR(11) PRIMARY KEY, nombre_tripulante CHAR(50) NOT NULL, campo CHAR(10) NOT NULL );";
 
 
+
+    //Triggers Vlad
+    private static final String crear_pasajero = "CREATE TRIGGER crear_pasajero\n" +
+            "AFTER INSERT ON Usuario\n" +
+            "FOR EACH ROW\n" +
+            "BEGIN\n" +
+            "  DECLARE count_pasajero INT;\n" +
+            "  \n" +
+            "  -- Verificar si ya existe un pasajero con el mismo ID\n" +
+            "  SELECT COUNT(*) INTO count_pasajero FROM Pasajero WHERE id_pasajero = NEW.id_usuario;\n" +
+            "  \n" +
+            "  -- Insertar un nuevo pasajero si no existe\n" +
+            "  IF count_pasajero = 0 THEN\n" +
+            "    INSERT INTO Pasajero (id_pasajero, nombre_pasajero, fecha_nacimiento, genero_pasajero)\n" +
+            "    VALUES (NEW.id_usuario, '', '', '');\n" +
+            "  END IF;\n" +
+            "END;";
+
+    private static final String evitar_duplicado_reclamo = "CREATE TRIGGER evitar_duplicado_reclamo\n" +
+            "BEFORE INSERT ON Reclamo\n" +
+            "FOR EACH ROW\n" +
+            "BEGIN\n" +
+            "  DECLARE count_reclamo INT;\n" +
+            "  \n" +
+            "  -- Verificar si ya existe un reclamo con el mismo ID\n" +
+            "  SELECT COUNT(*) INTO count_reclamo FROM Reclamo WHERE id_reclamo = NEW.id_reclamo;\n" +
+            "  \n" +
+            "  -- Cancelar la inserción si el ID ya existe\n" +
+            "  IF count_reclamo > 0 THEN\n" +
+            "    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No se puede insertar el reclamo. El ID ya está en uso.';\n" +
+            "  END IF;\n" +
+            "END;";
+
+    private static final String validar_fecha_pasajero = "CREATE TRIGGER validar_fecha_pasajero\n" +
+            "BEFORE INSERT ON Pasajero\n" +
+            "FOR EACH ROW\n" +
+            "BEGIN\n" +
+            "  IF NEW.fecha_nacimiento REGEXP '^[0-9]{2}/[0-9]{2}/[0-9]{4}$' = 0 THEN\n" +
+            "    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El formato de fecha para fecha_nacimiento debe ser dd/mm/yyyy.';\n" +
+            "  END IF;\n" +
+            "END;";
+
+    private static final String validar_fecha_reclamo="CREATE TRIGGER validar_fecha_reclamo\n" +
+            "BEFORE INSERT ON Reclamo\n" +
+            "FOR EACH ROW\n" +
+            "BEGIN\n" +
+            "  IF NEW.fecha_reclamo REGEXP '^[0-9]{2}/[0-9]{2}/[0-9]{4}$' = 0 THEN\n" +
+            "    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El formato de fecha para fecha_reclamo debe ser dd/mm/yyyy.';\n" +
+            "  END IF;\n" +
+            "END;";
+
+    private static final String validar_genero_pasajero = "CREATE TRIGGER validar_genero_pasajero\n" +
+            "BEFORE INSERT ON Pasajero\n" +
+            "FOR EACH ROW\n" +
+            "BEGIN\n" +
+            "  DECLARE genero_valido BOOLEAN;\n" +
+            "  \n" +
+            "  SET genero_valido = FALSE;\n" +
+            "  \n" +
+            "  IF NEW.genero_pasajero IN ('M', 'F', 'Masculino', 'Femenino') THEN\n" +
+            "    SET genero_valido = TRUE;\n" +
+            "  END IF;\n" +
+            "  \n" +
+            "  IF genero_valido = FALSE THEN\n" +
+            "    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El género del pasajero debe ser M, F, Masculino o Femenino.';\n" +
+            "  END IF;\n" +
+            "END;";
+
+    private static final String evitar_duplicado_id_reclamo ="CREATE TRIGGER evitar_duplicado_id_reclamo\n" +
+            "BEFORE INSERT ON Reclamo\n" +
+            "FOR EACH ROW\n" +
+            "BEGIN\n" +
+            "  DECLARE count_reclamo INT;\n" +
+            "  \n" +
+            "  -- Verificar si ya existe un reclamo con el mismo ID\n" +
+            "  SELECT COUNT(*) INTO count_reclamo FROM Reclamo WHERE id_reclamo = NEW.id_reclamo;\n" +
+            "  \n" +
+            "  -- Cancelar la inserción si el ID ya existe\n" +
+            "  IF count_reclamo > 0 THEN\n" +
+            "    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No se puede insertar el reclamo. El ID ya está en uso.';\n" +
+            "  END IF;\n" +
+            "END;";
+
+    private static final String validar_email_usuario="CREATE TRIGGER validar_email_usuario\n" +
+            "BEFORE INSERT ON Usuario\n" +
+            "FOR EACH ROW\n" +
+            "BEGIN\n" +
+            "  DECLARE valid_email BOOLEAN;\n" +
+            "\n" +
+            "  SET valid_email = FALSE;\n" +
+            "\n" +
+            "  IF NEW.email REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$' THEN\n" +
+            "    SET valid_email = TRUE;\n" +
+            "  END IF;\n" +
+            "\n" +
+            "  IF valid_email = FALSE THEN\n" +
+            "    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El formato del email no es válido.';\n" +
+            "  END IF;\n" +
+            "END;\n";
+
+    private static final String validar_estado_reclamo = "CREATE TRIGGER validar_estado_reclamo\n" +
+            "BEFORE INSERT ON Reclamo\n" +
+            "FOR EACH ROW\n" +
+            "BEGIN\n" +
+            "  DECLARE valid_estado BOOLEAN;\n" +
+            "\n" +
+            "  SET valid_estado = FALSE;\n" +
+            "\n" +
+            "  IF NEW.estado IN ('activo', 'cancelado', 'solucionado', 'terminado') THEN\n" +
+            "    SET valid_estado = TRUE;\n" +
+            "  END IF;\n" +
+            "\n" +
+            "  IF valid_estado = FALSE THEN\n" +
+            "    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El valor del estado no es válido. Los valores permitidos son: activo, cancelado, solucionado, terminado.';\n" +
+            "  END IF;\n" +
+            "END;";
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -68,6 +188,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(CREATE_TABLE_VUELO);
         db.execSQL(CREATE_TABLE_TRIPULACION_VUELO);
         db.execSQL(CREATE_TABLE_TRIPULANTE);
+
+        db.execSQL(CREATE_TABLE_AEROLINEA);
+
+
+        //triggers
+        /*
+        db.execSQL(crear_pasajero);
+        db.execSQL(evitar_duplicado_reclamo);
+        db.execSQL(validar_fecha_pasajero);
+        db.execSQL(validar_fecha_reclamo);
+        db.execSQL(validar_genero_pasajero);
+        db.execSQL(evitar_duplicado_id_reclamo);
+        db.execSQL(validar_email_usuario);
+        */
+
         db.execSQL(CREATE_TABLE_CUPO);
     }
 

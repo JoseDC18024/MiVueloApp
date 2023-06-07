4322368b8486fc7c7843cab19538e50152079db3
Jose Esteban Duran <dc18024@ues.edu.sv>
2023-06-07 16:19:13 -0600

    NUEVAS FUNCIONALIDADES

--- app/src/main/java/com/example/mivueloapp/Funcionalidades/LectorQRActivity.java
+++ app/src/main/java/com/example/mivueloapp/Funcionalidades/LectorQRActivity.java
@@ -1,9 +1,12 @@
 package com.example.mivueloapp.Funcionalidades;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.mivueloapp.R;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.Result;
@@ -11,13 +14,13 @@ import com.journeyapps.barcodescanner.BarcodeCallback;
 import com.journeyapps.barcodescanner.BarcodeResult;
 import com.journeyapps.barcodescanner.CompoundBarcodeView;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
-
 import java.util.List;
 
 public class LectorQRActivity extends AppCompatActivity implements BarcodeCallback {
 
     private CompoundBarcodeView barcodeScanner;
     private TextView resultTextView;
+    private Button visitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +29,17 @@ public class LectorQRActivity extends AppCompatActivity implements BarcodeCallba
 
         barcodeScanner = findViewById(R.id.barcodeScanner);
         resultTextView = findViewById(R.id.resultTextView);
+        visitButton = findViewById(R.id.visitButton);
 
         barcodeScanner.decodeContinuous(this);
+
+        visitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String scannedUrl = resultTextView.getText().toString();
+                openWebsite(scannedUrl);
+            }
+        });
     }
 
     @Override
@@ -50,5 +62,12 @@ public class LectorQRActivity extends AppCompatActivity implements BarcodeCallba
         }
     }
 
-}
 
+    private void openWebsite(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+}
